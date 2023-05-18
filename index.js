02a821cf24108b56c5b4c8daac86d6f6ee773f5b
filippo-ceradini <90599585+filippo-ceradini@users.noreply.github.com>
2023-05-18 14:20:33 +0200

    renamed app.js into index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import express from "express";
-//import session from "express-session";
+import session from "express-session";
 import dotenv from "dotenv";
 //import crypto from 'express';
 import mongoose from 'mongoose';
 import router from './router/router.js'
 
 dotenv.config()
+app.use(session({
+    secret: crypto.randomBytes(20).toString('hex'), // generate a random secret
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true } //'secure : true' expect us to use https
+}));
+
+import cors from "cors";
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:8080','http://10.0.1.3:8080']
+}));
 
 const index = express();
 index.use(express.json());
