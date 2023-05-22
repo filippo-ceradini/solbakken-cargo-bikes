dff8aa25a75f5a3b683018506fb8ce959bb09b86
filippo-ceradini <90599585+filippo-ceradini@users.noreply.github.com>
2023-05-22 07:34:43 +0200

    Fixed routes.js

--- routes.js
+++ routes.js
@@ -3,9 +3,7 @@ import UserVerification from "./database/models/UserVerification.js";
 import bcrypt from "bcrypt";
 import User from "./database/models/Users.js";
 import {sendEmailWithPhoto} from "./utils/mailSender.js";
-import multer from "multer";
 import fs from "fs";
-const upload = multer({ dest: 'uploads/' });
 
 const router = Router();
 
