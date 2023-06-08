abdb6aa5217eed888e7fa10c99a05e818af19d35
filippo-ceradini <90599585+filippo-ceradini@users.noreply.github.com>
2023-06-08 09:07:45 +0200

    Final fixes, login through express

--- utils/sessionMiddleware.js
+++ utils/sessionMiddleware.js
@@ -8,14 +8,14 @@ const mongoStore = MongoStore.create({ mongoUrl: process.env.MONGO_URI });
 const sessionMiddleware = session({
     secret: process.env.SESSION_SECRET,
     store: mongoStore,
-    resave: true,
+    resave: false,
     saveUninitialized: true,
     cookie: {
         cookie: { secure: false },
         httpOnly: false,
         expires: 1000 * 60 * 60 * 24 * 7, // 1 week
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: process.env.NODE_ENV,
+        sameSite: process.env.NODE_ENV,
     },
 });
 
